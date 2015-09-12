7861d94f36afb90a00b7845217eb03bf565f36be
yianyue <yianyue@gmail.com>
2015-09-12 19:50:45 -0400

    passed the one mocha test

--- upvotes.js
+++ upvotes.js
@@ -1,9 +1,12 @@
 exports.processData = function(n, k, votes) {    
   var i, j, l;
+  var result = [];
   for(i = 0; i < n-k+1; i++){
     var str = '';
     var arr = [votes[i]];
-    var nonDecr = 0;    
+    var arr2 = [votes[i]];
+    var nonDecr = 0;
+    var nonIncr = 0;    
 
     for(j = i; j < i+k-1; j++){
       // Non-decreasing
@@ -12,15 +15,21 @@ exports.processData = function(n, k, votes) {
       } else {
         // Total number of non-decreasing subset = S(S−1)/2 
         nonDecr += (arr.length - 1)*arr.length/2;
-        console.log(arr);
         arr = [votes[j+1]];
       };
       // Non-increasing
-      
+      if(votes[j] >= votes[j+1]){
+        arr2.push(votes[j+1]);
+      } else {
+        nonIncr += (arr2.length - 1)*arr2.length/2;
+        arr2 = [votes[j+1]];
+      };
+
     }
     nonDecr += (arr.length - 1)*arr.length/2;
-    console.log(arr, nonDecr);
+    nonIncr += (arr2.length - 1)*arr2.length/2;
+    result.push(nonDecr - nonIncr);
   }
-  return "something";
+  return result;
   // [3,0,-2];
 }
